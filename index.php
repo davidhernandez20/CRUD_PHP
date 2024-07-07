754c02c2c5a875469be258742a171d86fc81d115
davidhernandez20 <luisdavidhernandez57@gmail.com>
2024-07-07 12:19:14 -0500

    se realiza funcion para eliminar usuarios, y se empieza a agregar estilos

--- index.php
+++ index.php
@@ -25,25 +25,26 @@ $query = mysqli_query($con, $sql);
             href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css"
             rel="stylesheet"
             integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN"
-            crossorigin="anonymous"
-        />
+            crossorigin="anonymous"/>
+            <link rel="stylesheet" href="./styles_css/index.css">
     </head>
 
     <body>
-        <div>
-            <form action="insert_usuarios.php" method="POST">
+        <!-- Formulario de creacion de usuarios -->
+        <div class="formulario">
+            <form class="form" action="insert_usuarios.php" method="POST">
                 <h1>Crear Usuario</h1>
-                <input type="text" name="name"placeholder="Nombre">
-                <input type="text" name="lastname" placeholder="Apellido">
-                <input type="text" name="username" placeholder="Usuario">
-                <input type="text" name="password" placeholder="Contraseña">
-                <input type="text" name="email" placeholder="Correo">
+                <input class="input" type="text" name="name"placeholder="Nombre">
+                <input class="input" type="text" name="lastname" placeholder="Apellido">
+                <input class="input" type="text" name="username" placeholder="Usuario">
+                <input class="input" type="text" name="password" placeholder="Contraseña">
+                <input class="input" type="text" name="email" placeholder="Correo">
 
-                <input type="submit" value="Agregar Usuario">
+                <input class="input" type="submit" value="Agregar Usuario">
             </form>
         </div>
-
-        <div>
+        <!-- Tabla de usuarios registrados -->
+        <div class="tabla_registro">
             <h2>Usuarios Registrados</h2>
 
             <table>
@@ -59,7 +60,7 @@ $query = mysqli_query($con, $sql);
                         <th></th>
                     </tr>
                 </thead>
-
+                <!-- Tabla de usuarios con siclo while -->
                 <tbody>
                     <?php while($row = mysqli_fetch_array($query)): ?>
                     <tr>
@@ -69,6 +70,8 @@ $query = mysqli_query($con, $sql);
                     <th><?= $row ['lastname']?></th>
                     <th><?= $row ['password']?></th>
                     <th><?= $row ['email']?></th>
+
+                    <!-- Botones editar y eliminar -->
                     
                     <th><a href="editar.php?id=<?=$row ['id']?>">Editar</a></th>
                     <th><a href="eliminar.php?id=<?=$row ['id']?>">Eliminar</a></th>
